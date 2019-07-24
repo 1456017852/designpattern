6cebed3b66d4ecec2567dfd25bfed9abb6af3007
naruto <1456017852@qq.com>
2019-07-24 23:50:01 +0800

    单例模式:饿汉式(静态常量)

--- src/main/java/com/naruto/designpattern/singleton/hungry/staticfinal/Singleton.java
+++ src/main/java/com/naruto/designpattern/singleton/hungry/staticfinal/Singleton.java
@@ -1,7 +1,7 @@
-package com.naruto.designpattern.singleton.lazy.staticfinal;
+package com.naruto.designpattern.singleton.hungry.staticfinal;
 
 /**
- *  【单例模式:懒汉式(静态常量)】
+ *  【单例模式:饿汉式(静态常量)】
  *
  * 1、优点：类装载时完成对象的实例化(类的准备阶段),避免线程同步问题
  * 2、缺点：没有达到Lazy Loading的效果。如果从始至终从未使用过这个实例,则会造成内存的浪费
@@ -18,7 +18,7 @@ public class Singleton {
     private Singleton(){}
 
     //2、静态常量实例对象
-    public final static Singleton instance=new Singleton();
+    private final static Singleton instance=new Singleton();
 
     //3、公共的静态方法返回实例对象
     public static Singleton getInstance(){
