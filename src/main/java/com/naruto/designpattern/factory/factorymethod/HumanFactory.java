145ab827e6b2d058fa3b6676db6035b30a7ed166
naruto <1456017852@qq.com>
2019-07-31 23:04:05 +0800

    工厂方法模式

--- src/main/java/com/naruto/designpattern/factory/factorymethod/HumanFactory.java
+++ src/main/java/com/naruto/designpattern/factory/factorymethod/HumanFactory.java
@@ -1,7 +1,8 @@
 package com.naruto.designpattern.factory.factorymethod;
 
 /**
- * 工厂方法类
+ * 【工厂方法模式】
+ * 定义了一个创建对象的抽象方法，由子类决定要实例化的类。工厂方法模式将对象的实例化推迟到子类。
  */
 public class HumanFactory extends AbstractHumanFactory {
 
